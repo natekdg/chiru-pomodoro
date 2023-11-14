29b19aa61e5a7203b2d4e56a6f6de58de206f2f2
nathan <nathankdgray@gmail.com>
2023-11-13 21:49:03 -0800

    js file update
    - started adding js button elements
    - adding functions for buttons and creating algo for timing/notifications

---
+++ chirus-script.js
@@ -0,0 +1,28 @@
+let isRunning = false;
+let time;
+let timeRemaining = 25 * 60; // 25 minutes
+
+const timeDisplay = document.getElementById('time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+
+function updateDisplay() {
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    timeDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function tick() {
+    if (timeRemaining > 0) {
+        timeRemaining0--;
+        updateDsiplay():
+    } else {
+        stopTimer();
+        alert('Timer is done!');
+    }
+}
